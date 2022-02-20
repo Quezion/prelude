8e65ae4f40bbdb7800c97ad416363e703420ba41
Quest Yarbrough (*) <qyarbrough@GR-KCX30HW4Vmbp.local>
2022-02-20 14:39:07 -0800

    Update .gitignore to not include personal/custom.el

---
+++ personal/custom.el
@@ -0,0 +1,64 @@
+;; Required for GUI EmacsClient to connect
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+(setq auto-save-default nil) ;; stop files from autosaving
+
+;; --- GENERIC GUI WINDOW SETTINGS ---
+(setq ns-use-native-fullscreen nil)
+(setq ns-auto-hide-menu-bar t)
+(setq frame-resize-pixelwise t)
+
+(global-set-key (kbd "C-x f") 'toggle-frame-fullscreen)
+(add-hook 'after-init-hook (lambda () (toggle-frame-maximized)))
+
+;; --- LOAD /OS/ SPECIFIC SETTINGS ---
+(load-file (expand-file-name
+            (cond ((eq system-type 'windows-nt) "windows.el")
+                  ((eq system-type 'darwin) "mac.el")
+                  (t "default-system.el"))
+            (concat user-emacs-directory "/personal/os")))
+
+
+;; --- GENERIC EMACS SETTINGS ---
+(setq whitespace-line-column 97);; set when trailing lines will be highlighted
+
+;; Remove line-numbers after load (auto-setup by Prelude)
+(defun nolinum ()
+  (global-display-line-numbers-mode 0)
+  (global-linum-mode 0))
+(add-hook 'prog-mode-hook 'nolinum)
+
+;; Ensure grep results don't pull in extralong lines & slowdown
+(grep-compute-defaults)
+(grep-apply-setting 'grep-find-template
+                    (concat grep-find-template " | cut -c 1-2000"))
+
+;; --- CONFIGURE PROG MODES ---
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;; --- CUSTOM PACKAGE AUTOGEN GOBBLYGOOK ---
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bc393979763968cba41ca1366ae7ddb2510e034d60a2ef91d145c6d5ed9e3032" default))
+ '(package-selected-packages
+   '(prodigy zop-to-char zenburn-theme yaml-mode which-key web-mode volatile-highlights undo-tree tide super-save solarized-theme smartrep smartparens rainbow-mode rainbow-delimiters operate-on-number nlinum move-text magit lsp-ui key-chord json-mode js2-mode imenu-anywhere hl-todo helm-projectile helm-descbinds helm-ag guru-mode go-mode git-timemachine git-modes gist expand-region exec-path-from-shell elisp-slime-nav editorconfig easy-kill dockerfile-mode discover-my-major diminish diff-hl crux counsel company-anaconda cider buffer-move browse-kill-ring anzu ag ace-window)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; --- MISC ---
+
+;; NOTE: you can run this magic command in bash to generate eshell alias file
+;; alias | sed -E "s/^alias ([^=]+)='(.*)'$/alias \1 \2 \$*/g; s/'\\\''/'/g;" >~/.emacs.d/savefile/eshell/alias
+;; see https://www.emacswiki.org/emacs/EshellAlias
